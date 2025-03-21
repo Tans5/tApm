245dd6d85a78202a92be177c417e484ea488fdf0
anonymous <anonymous@inke.cn>
2025-03-21 13:29:56 +0800

    [feat] Update libbreakpad_client.a

--- tapm-core/src/main/cpp/jni.cpp
+++ tapm-core/src/main/cpp/jni.cpp
@@ -4,11 +4,30 @@
 
 #include <jni.h>
 #include "anr/anr.h"
+//#include "breakpad/client/linux/handler/exception_handler.h"
+//
+//// 崩溃回调函数（可选）
+//bool DumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
+//                  void* context,
+//                  bool succeeded) {
+//    return succeeded;
+//}
 
 extern "C" JNIEXPORT jlong JNICALL
 Java_com_tans_tapm_monitors_AnrMonitor_registerAnrMonitorNative(
         JNIEnv * env,
         jobject javaAnrMonitor) {
+
+//    google_breakpad::MinidumpDescriptor descriptor(0);
+//    google_breakpad::ExceptionHandler eh(
+//            descriptor,
+//            NULL,         // FilterCallback（可选）
+//            DumpCallback,  // 崩溃后的回调
+//            NULL,          // 回调上下文
+//            true,          // 是否启动子进程处理崩溃
+//            -1             // 文件描述符（默认 -1）
+//    );
+
     auto anr = new Anr;
     auto ret = anr->prepare(env, javaAnrMonitor);
     if (ret == 0) {
