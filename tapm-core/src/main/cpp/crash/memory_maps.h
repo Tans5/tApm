8ff43cb09ad5d11856ef1f0ac3f17290b961fc2e
anonymous <anonymous@inke.cn>
2025-04-16 12:47:10 +0800

    [feat] Remove libunwind.

--- tapm-core/src/main/cpp/crash/memory_maps.h
+++ tapm-core/src/main/cpp/crash/memory_maps.h
@@ -8,7 +8,6 @@
 #include <cstdint>
 #include <sys/types.h>
 #include <../linkedlist/linked_list.h>
-#include "t_elf.h"
 #include "file_mmap.h"
 #include "../tapm_size.h"
 
@@ -34,13 +33,6 @@ typedef struct MemoryMap {
     uint64_t inode = 0;
     char pathname[MAX_STR_SIZE] {};
     bool isMapPortDevice = false;
-    T_Elf *elf = nullptr;
-    bool isLoadedElf = false;
-    // elf 文件在文件中的偏移，例如 apk 中的 so 库通常就有一个偏移；而单独文件的 so 就没有偏移。
-    addr_t elfFileStart = 0;
-    // elf 文件被加载到内存中的偏移部分，例如当前的位置只加载了 DYNAMIC 段，这个段在文件中的偏移量为 4096.
-    addr_t elfLoadedStart = 0;
-    Mapped * elfFileMap = nullptr;
     MemoryMap *previous = nullptr;
 } MemoryMap;
 
@@ -49,16 +41,6 @@ void parseMemoryMaps(pid_t pid, LinkedList *output);
 bool tryFindAbortMsg(pid_t pid, LinkedList *maps, char *output);
 
 bool findMemoryMapByAddress(addr_t address, LinkedList *maps, MemoryMap **target);
-
-bool tryLoadElfFileMmap(MemoryMap *memoryMap);
-
-bool tryLoadElf(MemoryMap *memoryMap);
-
-addr_t convertAddressToElfOffset(MemoryMap *memoryMap, addr_t address);
-
-bool loadElfSymbol(addr_t address, LinkedList *maps, MemoryMap **outputMap, addr_t *outputElfOffset, char *outputSymbol, addr_t *outputSymbolOffset) ;
-
-void recycleElfFileMap(MemoryMap *memoryMap);
 
 void recycleMemoryMaps(LinkedList *toRecycle);
 
